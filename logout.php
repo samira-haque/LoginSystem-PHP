56e74f6ecf01e2abcbf6706503e7eb3c0a66ebbe
samira haque <sh6007530@gmail.com>
2025-10-20 14:54:45 +0600

    php

--- logout.php
+++ logout.php
@@ -1,22 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+session_start();
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+session_unset();
+session_destroy();
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <?php require 'partials/_nav.php' ?>
+header("location: login.php");
+exit ;
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-</html>
+?>
--- partials/_nav.php
+++ partials/_nav.php
@@ -1,4 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<?php
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!==true){
+    $loggedin = true;
+}
+
+else{
+  $loggedin = false ;
+}
+echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/LoginSystem">iSecure</a>
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
@@ -10,21 +18,27 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/LoginSystem/welcome.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
+      </li>';
+      if(!$loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/LoginSystem/login.php">Login</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/LoginSystem/signup.php">Signup</a>
-      </li>
-      <li class="nav-item">
+      </li>' ;
+      }
+      if($loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/LoginSystem/logout.php">Logout</a>
-      </li>
-    </ul>
-
+      </li>';
+      }
+    
+    echo '</ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
-</nav>
+</nav>' ;
+
+?>
